25b21f1fb03737a5a516922778f0b43279e83906
Hamid Molareza <kitm.hamid@gmail.com>
2023-01-14 15:09:00 +0330

    refactor(1st.py): minor updates

--- 1st.py
+++ 1st.py
@@ -1,19 +1,20 @@
-print("welcome to my first game")
+print("welcome to my first game!\n")
+
 name = input("what is your name? ")
 age = int(input("How old are you? "))
 if age >= 18:
-    print("You are old enough to play the game")
-    want_to_play = input("do you want to play? ").lower()
+    print("You are old enough to play the game.")
+    want_to_play = input("do you want to play?(yes/no) ").lower()
     if want_to_play == "yes":
-        print("lets begin")
-        L_R = input("left or right? ")
-        if L_R == "left":
-            print("correct")
+        print("lets begin...")
+        user_answer = input("left or right? ")
+        if user_answer == "left":
+            print("correct!")
         else:
-            print("game over")
+            print("game over!")
     else:
         print("cya...")
 elif age >= 14:
-    print("you can play with your parents")
+    print("you can play with your parents.")
 else:
-    print("you are not old enough...")
+    print("you are not old enough.")
